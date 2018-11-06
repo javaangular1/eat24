39cd980bc7f22c873897a20b8620aedd637300a2
JavaScript-Sdet <orazovvepa@yahoo.com>
2018-11-06 00:10:23 -0600

    accepted change on conf.js

--- Configuration/conf.js
+++ Configuration/conf.js
@@ -9,11 +9,7 @@ exports.config = {
         browserName: 'chrome'
     },
 
-<<<<<<< HEAD
-    specs: ['../Tests/drivers.spec.js'],
-=======
     specs: ['../Tests/Eat24.spec.js'],
->>>>>>> 779857f93e7b4d2e44b627bb051a8e44fb1c854c
     onPrepare: function () {
         browser.driver.manage().window().maximize();
         jasmine.getEnv().addReporter(new SpecReporter({
